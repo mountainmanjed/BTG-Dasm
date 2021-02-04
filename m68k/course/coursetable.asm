8cdb87939433ab2900de1744ec4831e4308ed55f
jedpossum <kusoiggy@gmail.com>
2021-02-04 18:40:36 -0500

    first commit

---
+++ m68k/course/coursetable.asm
@@ -0,0 +1,479 @@
+loc_057b44:
+	dc.l loc_057b54;US
+	dc.l loc_057ee4;GR
+	dc.l loc_0580ac;JP
+	dc.l loc_057d1c;AU
+
+;==============================================
+;US_TABLE
+	org $57b54
+loc_057b54:
+;Hole 00
+	dc.l loc_020a64,loc_0a3e62
+	dc.w $00ff,$01d5
+	dc.l loc_0a3d8a
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 01
+	dc.l loc_020a64,loc_0a3e62
+	dc.w $0106,$01e8
+	dc.l loc_0a3d8a
+	dc.w $00f0,$01e3,$00d6,$01e3
+
+;Hole 02
+	dc.l loc_02168a,loc_0a3e9a
+	dc.w $0107,$01da
+	dc.l loc_0a3d96
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 03
+	dc.l loc_023332,loc_0a3ec2
+	dc.w $010b,$01d5
+	dc.l loc_0a3da2
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 04
+	dc.l loc_023af8,loc_0a3eda
+	dc.w $00ff,$01e9
+	dc.l loc_0a3dae
+	dc.w $00f4,$01e3,$00da,$01e3
+
+;Hole 05
+	dc.l loc_02543c,loc_0a3f20
+	dc.w $00fc,$01d5
+	dc.l loc_0a3dba
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 06
+	dc.l loc_026308,loc_0a3f54
+	dc.w $00f9,$01e6
+	dc.l loc_0a3dc6
+	dc.w $00f5,$01e3,$00db,$01e3
+
+;Hole 07
+	dc.l loc_0270d8,loc_0a3f8c
+	dc.w $00fe,$01d5
+	dc.l loc_0a3dd2
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 08
+	dc.l loc_027e5c,loc_0a3fbc
+	dc.w $0103,$01d5
+	dc.l loc_0a3dde
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 09
+	dc.l loc_029368,loc_0a4000
+	dc.w $00ff,$01d5
+	dc.l loc_0a3dea
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 10
+	dc.l loc_029eec,loc_0a4028
+	dc.w $00fb,$01d5
+	dc.l loc_0a3df6
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 11
+	dc.l loc_02abfe,loc_0a4054
+	dc.w $00f8,$01e7
+	dc.l loc_0a3e02
+	dc.w $00ef,$01e3,$00d5,$01e3
+
+;Hole 12
+	dc.l loc_02c126,loc_0a4096
+	dc.w $0107,$01d8
+	dc.l loc_0a3e0e
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 13
+	dc.l loc_02d396,loc_0a40ae
+	dc.w $00fa,$01e7
+	dc.l loc_0a3e1a
+	dc.w $00f4,$01e3,$00da,$01e3
+
+;Hole 14
+	dc.l loc_02e51e,loc_0a40ea
+	dc.w $0106,$01d7
+	dc.l loc_0a3e26
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 15
+	dc.l loc_02f2dc,loc_0a4118
+	dc.w $0101,$01d5
+	dc.l loc_0a3e32
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 16
+	dc.l loc_02fe1e,loc_0a413e
+	dc.w $0108,$01e6
+	dc.l loc_0a3e3e
+	dc.w $00f4,$01e3,$00da,$01e3
+
+;Hole 17
+	dc.l loc_030f20,loc_0a4166
+	dc.w $00fd,$01e8
+	dc.l loc_0a3e4a
+	dc.w $00e9,$01e3,$00cf,$01e3
+
+;Hole 18
+	dc.l loc_0328e2,loc_0a41ae
+	dc.w $010a,$01d5
+	dc.l loc_0a3e56
+	dc.w $0118,$01e3,$00fe,$01e3
+
+
+
+;==============================================
+;AU_TABLE
+	org $57d1c
+loc_057d1c:
+;Hole 00
+	dc.l loc_0334f6,loc_0a469c
+	dc.w $00fe,$01d5
+	dc.l loc_0a45c4
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 01
+	dc.l loc_0334f6,loc_0a469c
+	dc.w $0105,$01d5
+	dc.l loc_0a45c4
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 02
+	dc.l loc_0340ae,loc_0a46c0
+	dc.w $0106,$01d6
+	dc.l loc_0a45d0
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 03
+	dc.l loc_034da6,loc_0a46f2
+	dc.w $0107,$01d7
+	dc.l loc_0a45dc
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 04
+	dc.l loc_0356d6,loc_0a470a
+	dc.w $0105,$01d9
+	dc.l loc_0a45e8
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 05
+	dc.l loc_036624,loc_0a4748
+	dc.w $00fe,$01d6
+	dc.l loc_0a45f4
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 06
+	dc.l loc_03763c,loc_0a4778
+	dc.w $00fe,$01d5
+	dc.l loc_0a4600
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 07
+	dc.l loc_038516,loc_0a47a8
+	dc.w $00fd,$01d7
+	dc.l loc_0a460c
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 08
+	dc.l loc_03989e,loc_0a47ea
+	dc.w $0107,$01d5
+	dc.l loc_0a4618
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 09
+	dc.l loc_03aa6c,loc_0a4802
+	dc.w $00f9,$01d4
+	dc.l loc_0a4624
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 10
+	dc.l loc_03b9c4,loc_0a4836
+	dc.w $0105,$01d4
+	dc.l loc_0a4630
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 11
+	dc.l loc_03c516,loc_0a4860
+	dc.w $00f4,$01d8
+	dc.l loc_0a463c
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 12
+	dc.l loc_03efbc,loc_0a489c
+	dc.w $0109,$01d5
+	dc.l loc_0a4648
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 13
+	dc.l loc_03f960,loc_0a48b8
+	dc.w $0102,$01e8
+	dc.l loc_0a4654
+	dc.w $00f6,$01e3,$00dc,$01e3
+
+;Hole 14
+	dc.l loc_0407e0,loc_0a48e0
+	dc.w $0107,$01d7
+	dc.l loc_0a4660
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 15
+	dc.l loc_041582,loc_0a490a
+	dc.w $00fc,$01db
+	dc.l loc_0a466c
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 16
+	dc.l loc_042848,loc_0a494a
+	dc.w $0104,$01d6
+	dc.l loc_0a4678
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 17
+	dc.l loc_04327c,loc_0a496c
+	dc.w $00fa,$01d7
+	dc.l loc_0a4684
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 18
+	dc.l loc_043ea6,loc_0a499c
+	dc.w $00ff,$01d6
+	dc.l loc_0a4690
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;==============================================
+;GR_TABLE
+	org $57ee4
+loc_057ee4:
+;Hole 00
+	dc.l loc_044d54,loc_0a42a6
+	dc.w $0106,$01d5
+	dc.l loc_0a41ce
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 01
+	dc.l loc_044d54,loc_0a42a6
+	dc.w $0105,$01e7
+	dc.l loc_0a41ce
+	dc.w $00f1,$01e3,$00d7,$01e3
+
+;Hole 02
+	dc.l loc_045a1a,loc_0a42d8
+	dc.w $0107,$01e7
+	dc.l loc_0a41da
+	dc.w $00f2,$01e3,$00d8,$01e3
+
+;Hole 03
+	dc.l loc_0468d2,loc_0a4300
+	dc.w $010c,$01d3
+	dc.l loc_0a41e6
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 04
+	dc.l loc_0471c0,loc_0a4318
+	dc.w $0106,$01e7
+	dc.l loc_0a41f2
+	dc.w $00ef,$01e3,$00d5,$01e3
+
+;Hole 05
+	dc.l loc_047fae,loc_0a434c
+	dc.w $0104,$01e7
+	dc.l loc_0a41fe
+	dc.w $00f2,$01e3,$00d8,$01e3
+
+;Hole 06
+	dc.l loc_048fce,loc_0a4386
+	dc.w $0102,$01d7
+	dc.l loc_0a420a
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 07
+	dc.l loc_04a18e,loc_0a43b6
+	dc.w $0100,$01d5
+	dc.l loc_0a4216
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 08
+	dc.l loc_04b3c6,loc_0a43f0
+	dc.w $010c,$01d5
+	dc.l loc_0a4222
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 09
+	dc.l loc_04bc2e,loc_0a4408
+	dc.w $010a,$01d9
+	dc.l loc_0a422e
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 10
+	dc.l loc_04caca,loc_0a442c
+	dc.w $00fd,$01da
+	dc.l loc_0a423a
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 11
+	dc.l loc_04df62,loc_0a4468
+	dc.w $0105,$01d5
+	dc.l loc_0a4246
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 12
+	dc.l loc_04e73e,loc_0a4480
+	dc.w $0102,$01d5
+	dc.l loc_0a4252
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 13
+	dc.l loc_04f7d8,loc_0a44b0
+	dc.w $0106,$01d5
+	dc.l loc_0a425e
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 14
+	dc.l loc_050130,loc_0a44d4
+	dc.w $010a,$01d5
+	dc.l loc_0a426a
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 15
+	dc.l loc_0507f8,loc_0a44ec
+	dc.w $0106,$01d3
+	dc.l loc_0a4276
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 16
+	dc.l loc_0516b8,loc_0a451c
+	dc.w $00fc,$01d6
+	dc.l loc_0a4282
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 17
+	dc.l loc_05257a,loc_0a4552
+	dc.w $00fc,$01e5
+	dc.l loc_0a428e
+	dc.w $00f2,$01e3,$00d8,$01e3
+
+;Hole 18
+	dc.l loc_053962,loc_0a4596
+	dc.w $0104,$01d3
+	dc.l loc_0a429a
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;==============================================
+;JP_TABLE
+	org $580ac
+loc_0580ac:
+;Hole 00
+	dc.l loc_00f4fa,loc_0a3a68
+	dc.w $0101,$01d6
+	dc.l loc_0a3990
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 01
+	dc.l loc_00f4fa,loc_0a3a68
+	dc.w $0109,$01d7
+	dc.l loc_0a3990
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 02
+	dc.l loc_010170,loc_0a3a8c
+	dc.w $0107,$01e7
+	dc.l loc_0a399c
+	dc.w $00f2,$01e3,$00d8,$01e3
+
+;Hole 03
+	dc.l loc_010f38,loc_0a3abc
+	dc.w $010b,$01d6
+	dc.l loc_0a39a8
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 04
+	dc.l loc_011900,loc_0a3ad8
+	dc.w $0102,$01d5
+	dc.l loc_0a39b4
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 05
+	dc.l loc_0132ce,loc_0a3b06
+	dc.w $00fa,$01e7
+	dc.l loc_0a39c0
+	dc.w $00fa,$01e3,$00e0,$01e3
+
+;Hole 06
+	dc.l loc_0146d4,loc_0a3b3e
+	dc.w $0107,$01d6
+	dc.l loc_0a39cc
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 07
+	dc.l loc_015202,loc_0a3b5a
+	dc.w $0104,$01d4
+	dc.l loc_0a39d8
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 08
+	dc.l loc_0160ec,loc_0a3b8c
+	dc.w $0102,$01e4
+	dc.l loc_0a39e4
+	dc.w $00ed,$01e3,$00d3,$01e3
+
+;Hole 09
+	dc.l loc_017458,loc_0a3bc8
+	dc.w $0108,$01d6
+	dc.l loc_0a39f0
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 10
+	dc.l loc_017bee,loc_0a3bea
+	dc.w $0105,$01d6
+	dc.l loc_0a39fc
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 11
+	dc.l loc_018a66,loc_0a3c1c
+	dc.w $0101,$01e8
+	dc.l loc_0a3a08
+	dc.w $00f3,$01e3,$00d9,$01e3
+
+;Hole 12
+	dc.l loc_01a1fc,loc_0a3c5a
+	dc.w $0101,$01e7
+	dc.l loc_0a3a14
+	dc.w $00f4,$01e3,$00da,$01e3
+
+;Hole 13
+	dc.l loc_01b038,loc_0a3c92
+	dc.w $0101,$01d5
+	dc.l loc_0a3a20
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 14
+	dc.l loc_01bcb4,loc_0a3cbc
+	dc.w $010a,$01d3
+	dc.l loc_0a3a2c
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 15
+	dc.l loc_01c4c6,loc_0a3cd4
+	dc.w $0100,$01d6
+	dc.l loc_0a3a38
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 16
+	dc.l loc_01d35a,loc_0a3d02
+	dc.w $00f8,$01e6
+	dc.l loc_0a3a44
+	dc.w $00ee,$01e3,$00d4,$01e3
+
+;Hole 17
+	dc.l loc_01e722,loc_0a3d44
+	dc.w $0107,$01d2
+	dc.l loc_0a3a50
+	dc.w $0118,$01e3,$00fe,$01e3
+
+;Hole 18
+	dc.l loc_01fc32,loc_0a3d5c
+	dc.w $0105,$01d3
+	dc.l loc_0a3a5c
+	dc.w $0118,$01e3,$00fe,$01e3
