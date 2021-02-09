774b941840365d8a12f57fd01d9a4e761c82205d
jedpossum <kusoiggy@gmail.com>
2021-02-09 18:45:39 -0500

    update

--- m68k/gamevalues.asm
+++ m68k/gamevalues.asm
@@ -45,19 +45,14 @@ unk_102b84 = 0x102b84
 unk_102b88 = 0x102b88
 
 unk_102c38 = 0x102c38
-unk_102c4c = 0x102c4c
-unk_102c68 = 0x102c68
-unk_102c6c = 0x102c6c
-unk_102c70 = 0x102c70
 
 unk_102c39 = 0x102c39
 unk_102c3a = 0x102c3a
 unk_102c3b = 0x102c3b
-unk_102c3c = 0x102c3c
-
-unk_102c40 = 0x102c40
-unk_102c44 = 0x102c44
-unk_102c48 = 0x102c48
+unk_102c3c = 0x102c3c ; Ball Velocity Velocity
+unk_102c40 = 0x102c40 ; Ball Velocity X
+unk_102c44 = 0x102c44 ; Ball Velocity Y
+unk_102c48 = 0x102c48 ; Ball Velocity Z
 unk_102c4c = 0x102c4c
 unk_102c50 = 0x102c50
 unk_102c54 = 0x102c54
@@ -65,33 +60,56 @@ unk_102c58 = 0x102c58
 unk_102c5c = 0x102c5c
 unk_102c60 = 0x102c60
 unk_102c64 = 0x102c64
+unk_102c68 = 0x102c68;Starting Postion X
+unk_102c6c = 0x102c6c;Starting Postion Y
+unk_102c70 = 0x102c70;Starting Postion Z
 
-unk_102c80 = 0x102c80
-unk_102c84 = 0x102c84
+unk_102c7c = 0x102c7c
+unk_102c80 = 0x102c80 ;VS Velocity Sin
+unk_102c84 = 0x102c84 ;VC Velocity Cosine
 unk_102c88 = 0x102c88
 unk_102c8c = 0x102c8c
 unk_102c90 = 0x102c90
 unk_102c94 = 0x102c94
 unk_102c98 = 0x102c98
 unk_102c9c = 0x102c9c
+
+unk_102ca8 = 0x102ca8
+unk_102cac = 0x102cac
+unk_102cb0 = 0x102cb0
 unk_102cbc = 0x102cbc
 unk_102cc0 = 0x102cc0
 unk_102cc4 = 0x102cc4
 unk_102cc8 = 0x102cc8
 unk_102ccc = 0x102ccc
 
-
 unk_102cd0 = 0x102cd0
+unk_102cd2 = 0x102cd2
 unk_102cd6 = 0x102cd6
-
+unk_102cd8 = 0x102cd8
+unk_102cdc = 0x102cdc
+unk_102ce0 = 0x102ce0
+unk_102ce4 = 0x102ce4
 unk_102ce8 = 0x102ce8
 unk_102cea = 0x102cea
+unk_102cec = 0x102cec
 unk_102cee = 0x102cee
 unk_102cef = 0x102cef
 unk_102cf0 = 0x102cf0
 unk_102cf1 = 0x102cf1
 unk_102cf2 = 0x102cf2
 
+unk_102cf6 = 0x102cf6 ;DS
+unk_102cfa = 0x102cfa ;DD
+
+unk_102d02 = 0x102d02
+unk_102d06 = 0x102d06
+unk_102d0a = 0x102d0a
+unk_102d12 = 0x102d12
+unk_102d16 = 0x102d16
+unk_102d1a = 0x102d1a
+unk_102d1e = 0x102d1e
+unk_102d22 = 0x102d22
 unk_102d26 = 0x102d26
 unk_102d28 = 0x102d28
 unk_102d29 = 0x102d29
@@ -99,6 +117,7 @@ unk_102d2a = 0x102d2a
 unk_102d2e = 0x102d2e
 
 unk_102d32 = 0x102d32
+unk_102d36 = 0x102d36
 unk_102d3a = 0x102d3a
 unk_102db6 = 0x102db6
 
@@ -120,6 +139,8 @@ unk_106fe8 = 0x106fe8
 unk_107076 = 0x107076;Player Data Pointer
 unk_10707a = 0x10707a;Course Data Pointer
 unk_107082 = 0x107082
+unk_107083 = 0x107083
+unk_107084 = 0x107084
 
 unk_107090 = 0x107090;Course Data Tilemap
 
@@ -130,9 +151,12 @@ unk_10d4c2 = 0x10d4c2
 unk_10d4c4 = 0x10d4c4
 unk_10d4c6 = 0x10d4c6
 unk_10d4ca = 0x10d4ca
+unk_10d4ce = 0x10d4ce
 
 unk_10d4d2 = 0x10d4d2
 unk_10d4d4 = 0x10d4d4
+unk_10d4d6 = 0x10d4d6
+
 
 unk_10d4f8 = 0x10d4f8
 unk_10d4fa = 0x10d4fa
